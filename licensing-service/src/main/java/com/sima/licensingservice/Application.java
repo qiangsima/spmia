026634e66a2d2ac2724e14c8a6b6d870b80b9694
qiangsima <qsima@seu.edu.cn>
2019-01-04 11:10:58 +0800

    registry licensingservice to eurekasvr

--- licensing-service/src/main/java/com/sima/licensingservice/Application.java
+++ licensing-service/src/main/java/com/sima/licensingservice/Application.java
@@ -2,11 +2,13 @@ package com.sima.licensingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Created by qisima on 12/27/2018 3:28 PM
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @RefreshScope
 public class Application {
